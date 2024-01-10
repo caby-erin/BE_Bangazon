66b4ea6327103296bf6870679640953e1f464852
caby-erin <caby27@gmail.com>
2024-01-09 19:26:19 -0600

    models built, data seeded, user and order views are working

---
+++ bangazonapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .item import Item
+from .order import Order
+from .order_item import OrderItem
+from .revenue import Revenue
+from .user import User
---
+++ bangazonapi/models/order.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .user import User
+
+class Order(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+  name = models.CharField(max_length=50)
+  is_closed = models.BooleanField()
+  customer_phone = models.CharField(max_length=50)
+  customer_email = models.CharField(max_length=50)
+  order_type = models.CharField(max_length=50)
